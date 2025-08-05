6a4d5f4e442eb998e3eb1660c0487ba2c8164670
yane <jersoto@scl.edu.gt>
2025-08-05 18:40:04 +0200

    bbaner

--- componentes/Banner/Banner.js
+++ componentes/Banner/Banner.js
@@ -1,15 +1,15 @@
 export function banner(){
-    let div = document.createElement("div");
-    div.className="div-banner";
+     let div = document.createElement("div");
+    div.className = "div-Banner"; 
 
-    let span= document.createElement("span");
-    span.textContent = "Geeta"
+    let span = document.createElement("span");
+    span.textContent = "Geeta";
     div.appendChild(span);
 
     let aBoton = document.createElement("a");
-    aBoton.className = "a-boton";
-   aBoton.href = "componentes/login/login.html";
-    aBoton.textContent="SHOP NOW"
+    aBoton.className = "aboton"; 
+    aBoton.href = "componentes/login/login.html";
+    aBoton.textContent = "SHOP NOW";
     div.appendChild(aBoton);
 
     return div;
---
+++ componentes/login/login.js
@@ -0,0 +1,8 @@
+export function Login(){
+    let div = document.createElement("div");
+    div.className="login";
+     
+
+    return Login;
+}
+document.body.appendChild(Login());
